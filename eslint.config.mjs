aceb87bde2b75eb975c240a4fcf19a8f7e0daf3b
Gary Zhang <122765769+mathsonline-gary@users.noreply.github.com>
2025-05-21 15:32:05 +1000

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,57 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import importPlugin from 'eslint-plugin-import'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier'
+  ),
+  {
+    plugins: {
+      importPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {},
+        node: true,
+      },
+    },
+    rules: {
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'import/no-unresolved': 'error',
+      'import/prefer-default-export': 'off',
+      'import/no-default-export': 'warn',
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
